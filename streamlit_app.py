b0bb52b31f353c3137e6f86f0437c34f7f74709e
wongwara.w <wongwara.w@gmail.com>
2024-04-22 18:16:56 +1000

    add title

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ def create_pharmacy_map(user_location, nearest_pharmacies):
 
 # Main Streamlit function
 def main():
-    st.title("Streamlit Chat")
+    st.title("Oversea Student Health Chatbot")
     greeting = "Hi, how can I help you? Choose from menu items Diagnosis, OSHC, or Pharmacy Location."
     information = 'This application is demo application for chatbot. You can ask for the Pharmacy Location.'
 
